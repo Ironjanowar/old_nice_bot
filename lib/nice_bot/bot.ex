7a211ef3d703500a76f4964d1944b73dcbd803cd
César <Ironjanowar@users.noreply.github.com>
2018-03-30 02:28:49 +0200

    Fixed bot

--- lib/nice_bot/bot.ex
+++ lib/nice_bot/bot.ex
@@ -3,6 +3,7 @@ defmodule NiceBot.Bot do
   def bot(), do: @bot_name
 
   use Telex.Bot, name: @bot_name
+  use Telex.Dsl
 
   def handle({:command, "start", msg}, _name, _extra) do
     answer("Hi! I'm a nice bot :D")
